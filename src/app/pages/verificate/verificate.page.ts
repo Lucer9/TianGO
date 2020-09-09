eb8f8d91fd440010e1c55fee89ee47d90560364c
Ricardo <A01337569@itesm.mx>
2020-09-09 03:31:28 -0500

    OwO ta el del código y el de verificación

---
+++ src/app/pages/verificate/verificate.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verificate',
+  templateUrl: './verificate.page.html',
+  styleUrls: ['./verificate.page.scss'],
+})
+export class VerificatePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
